8ac29fe91757fe1f71c39f491d994a356bc41fc2
Speed Force <122682099+speedforce-lk@users.noreply.github.com>
2023-09-06 13:33:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install docker.io
 sudo apt install docker-compose
-git clone https://github.com/speedforce-demo/seedbox-with-caddy
+git clone https://github.com/speedforce-lk/seedbox-with-caddy
 cd transmission-seedbox
 docker-compose up -d
